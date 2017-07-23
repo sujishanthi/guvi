eacd5166840762d66e53923267a86a9432daaf42
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-07-23 13:26:58 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i,n1,n2,gcd;
+printf("enter the numbers");
+scanf("%d%d",&n1,&n2);
+for(i=1;i<=n1&&i<=n2;i++)
+{
+if(n1%i==0&&n2%==0)
+{
+gcd=i;
+}
+}
+printf(i);
+}
