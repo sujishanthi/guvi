523e693cd8e8301f40f24cb147f43ccb6ff0f1cd
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-07-24 09:53:19 +0530

    Update primenum.c

--- primenum.c
+++ primenum.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-int main()
+void main()
 {
-    int low, high, i, flag;
-    printf("Enter two numbers(intervals): ");
-    scanf("%d %d", &low, &high);
-
+ int low, high, i, flag;
+  printf("Enter two numbers(intervals): ");
+  scanf("%d %d", &low, &high);
     printf("Prime numbers between %d and %d are: ", low, high);
-
     while (low < high)
     {
         flag = 0;
-
         for(i = 2; i <= low/2; ++i)
         {
             if(low % i == 0)
@@ -22,9 +19,6 @@ int main()
 
         if (flag == 0)
             printf("%d ", low);
-
         ++low;
     }
-
-    return 0;
 }
