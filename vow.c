8d27b56ffc6d0bb745d643cf6d431cc4de404a58
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-07-24 14:19:04 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char i,j,c;
+printf("enter the value");
+scanf("%c",c);
+i=('c==a','c==e','c==i','c==o','c==u');
+j=('c==A','c==E','c==I','c==O','c==U');
+if(i||j)
+printf("vowels");
+else
+printf("consonant");
+}
