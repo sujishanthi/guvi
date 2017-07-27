d81d57ec6787d06588cccc040953f0dd897fbea4
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-07-27 12:07:07 +0530

    Create days.c

---
+++ days.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+char days;
+scanf(%c",days);
+switch(days)
+{
+case 1:
+printf("monday");
+break;
+case 2:
+printf("tuesday");
+break;
+case 3:
+printf("wednesday");
+break;
+case 4:
+printf("thursday");
+break;
+case 5:
+printf("friday");
+break;
+case 6:
+printf("saturday");
+break;
+default:
+printf("sunday");
+break;
+}
+if(days==case)
+printf("yes");
+else
+printf("no");
+}
