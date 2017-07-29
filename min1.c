aab821ffe35f6c110ce791fbac172012be633707
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-07-29 16:51:44 +0530

    Rename min.c to min1.c

---
+++ min1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int arr[10],i;
+printf("enter the array");
+for(i=0;i<3;i++)
+scanf("%d",&arr[i]);
+if((arr[i]>arr[i+1])&&(arr[i]>arr[i+2]))
+printf("maximum number");
+else if((arr[i+1]>arr[i+2])&&(arr[i+1]>arr[i]))
+printf("minimum number");
+else
+printf("number");
+
+}
