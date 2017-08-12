f30672ca3ceb0031217831f6aad7b56f6e5dcb0a
sujishanthi <30317798+sujishanthi@users.noreply.github.com>
2017-08-12 16:53:12 +0530

    Create 52pgm.java

---
+++ 52pgm.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.scanner;
+public class number
+{
+public static void main(string args[])
+{
+scanner sc=new scanner(system.in);
+system.out.println("Enter the number");
+int number=sc.nextint();
+system.out.println("Enter the word");
+string word=sc.next();
+system.out.println("number"+number+"word"+word);
+sc.close();
+}
+}
